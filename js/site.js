3619e184f6f56a205b7aaa23224321e7556d462a
ShawnRogoff <shawnrogoff@gmail.com>
2021-07-29 09:51:10 -0500

    Updated layout on app page

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World!");
+// variables by id:
+// meleeProngSetting
+
+
+function toggleConfig(){
+    if (document.getElementById("configMenu").classList.contains("invisible")){
+        document.getElementById("configMenu").classList.remove("invisible");
+    } else {
+        document.getElementById("configMenu").classList.add("invisible");
+    }
 }
