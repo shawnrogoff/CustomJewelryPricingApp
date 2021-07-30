d0a5d258ba3f0858d9a38a8ebdd2934e5c051cc4
ShawnRogoff <shawnrogoff@gmail.com>
2021-07-29 22:21:41 -0500

    Updated button functionality; changed results output

--- js/site.js
+++ js/site.js
@@ -1,7 +1,4 @@
-// variables by id:
-// meleeProngSetting
-
-
+// toggle config options
 function toggleConfig(){
     if (document.getElementById("configMenu").classList.contains("invisible")){
         document.getElementById("configMenu").classList.remove("invisible");
@@ -9,3 +6,21 @@ function toggleConfig(){
         document.getElementById("configMenu").classList.add("invisible");
     }
 }
+// toggle melee pricing options
+function toggleMeleePricing(){
+    alert("You clicked to show melee pricing!");
+}
+// toggle melee pricing options
+function toggleCustomerGold(){
+    alert("You clicked to show customer gold pricing!");
+}
+
+// reset the form
+function resetForm(){
+    document.getElementById("alert").classList.add("invisible");
+}
+
+function getValues(){
+    document.getElementById("alert").classList.remove("invisible");
+}
+
